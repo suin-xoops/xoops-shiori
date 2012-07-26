df8fcf2e32b7e8070b40abeabee37d869ed4195b
Hidehito Nozawa aka Suin <suinyeze@gmail.com>
2012-07-27 02:13:08 +0900

    add shiori 1.01

--- html/modules/shiori/javascript/bookmark.js
+++ html/modules/shiori/javascript/bookmark.js
@@ -1,68 +1,81 @@
-$(document).ready(function(){
-	var star = $("#shiori_bookmark_star");
-	var star_url = star.attr("src");
-	var xoops_url = star_url.replace(/\/modules\/.*/, '');
-	var image_url = star_url.replace(/\/(un)*bookmarked\.png$/, '');
-	var shiori_dir = star_url.replace(/^.+\/modules\/([^\/]+)\/.*$/, '$1');
-	var shiori_url = xoops_url + '/modules/' + shiori_dir;
-	var title = document.title;
-	var url = location.href;
-
-	$.post(shiori_url + '/index.php?controller=ajax', { url: url },
-		function(data){
-			if ( data == 1 )
-			{
-				star.attr("src", image_url + '/bookmarked.png');
-			}
-			star.show();
-		},
-		'json'
-	);
-
-
-	star.click(function(){
+if(typeof jQuery != "undefined"){
+	$(document).ready(function(){
+		var star = $("#shiori_bookmark_star");
+		var star_url = star.attr("src");
+		var xoops_url = star_url.replace(/\/modules\/.*/, '');
+		var image_url = star_url.replace(/\/(un)*bookmarked\.png$/, '');
+		var shiori_dir = star_url.replace(/^.+\/modules\/([^\/]+)\/.*$/, '$1');
+		var shiori_url = xoops_url + '/modules/' + shiori_dir;
+		var title = document.title;
+		var url = location.href;
 	
-		if ( $("#shiori_bookmark_star").attr("src").match(/\/bookmarked\.png$/) )
-		{
-			var bookmarked = true;
-		}
-		else
-		{
-			var bookmarked = false;
-		}
-	
-		star.attr("src", image_url + '/loading.gif');
-
-		if ( bookmarked )
-		{
-			$.post(shiori_url + "/index.php?controller=ajax&action=ticket", {},
-				function(data){
-					$.post(shiori_url + "/index.php?controller=ajax&action=delete", { url: url, ticket: data },
-						function(data)
-						{
-							star.attr("src", image_url + '/unbookmarked.png');
-						},
-						"json"
-					);
-				},
-				"json"
-			);
-		}
-		else
-		{
-			$.post(shiori_url + "/index.php?action=form", { url: url, title: title },
-				function(data){
-					$.post(shiori_url + "/index.php?action=save",
-						$(data).find("#shiori_form").serialize(),
-						function(data)
-						{
-							star.attr("src", image_url + '/bookmarked.png');
-						}
-					);
+		$.post(shiori_url + '/index.php?controller=ajax', { url: url },
+			function(data){
+				if ( data == 1 )
+				{
+					star.attr("src", image_url + '/bookmarked.png');
 				}
-			);
-		}
-
-		return false;
+				star.show();
+			},
+			'json'
+		);
+	
+	
+		star.click(function(){
+		
+			if ( $("#shiori_bookmark_star").attr("src").match(/\/bookmarked\.png$/) )
+			{
+				var bookmarked = true;
+			}
+			else
+			{
+				var bookmarked = false;
+			}
+		
+			star.attr("src", image_url + '/loading.gif');
+	
+			if ( bookmarked )
+			{
+				$.post(shiori_url + "/index.php?controller=ajax&action=ticket", {},
+					function(data){
+						$.post(shiori_url + "/index.php?controller=ajax&action=delete", { url: url, ticket: data },
+							function(data)
+							{
+								if ( data == "ERROR" )
+								{
+									star.attr("src", image_url + '/error.png');
+									setTimeout(function(){
+										star.attr("src", image_url + '/bookmarked.png');
+									}, 1500
+									);
+								}
+								else
+								{
+									star.attr("src", image_url + '/unbookmarked.png');
+								}
+							},
+							"json"
+						);
+					},
+					"json"
+				);
+			}
+			else
+			{
+				$.post(shiori_url + "/index.php?action=form", { url: url, title: title },
+					function(data){
+						$.post(shiori_url + "/index.php?action=save",
+							$(data).find("#shiori_form").serialize(),
+							function(data)
+							{
+								star.attr("src", image_url + '/bookmarked.png');
+							}
+						);
+					}
+				);
+			}
+	
+			return false;
+		});
 	});
-});
+}
